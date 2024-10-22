0aa937346714cc6b6328753272e54efb6dbf739f
FionaGachuuri <fionairuka@gmail.com>
2024-10-22 22:31:22 +0300

    models

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,6 +3,9 @@
 that manages id attribute."""
 
 
+import json
+
+
 class Base:
     """Base class that manages 'id'
     attribute of future classes."""
@@ -19,3 +22,18 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Returns the JSON string representation of a list of dictionaries.
+
+        Args:
+            list_dictionaries (list):
+            A list of dictionaries to convert to JSON.
+
+        Returns:
+            str: The JSON string representation of list_dictionaries.
+        """
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
