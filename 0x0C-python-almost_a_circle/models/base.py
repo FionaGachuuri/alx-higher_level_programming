c054e7f5eba915b91c18f1d31812bf469096a58f
FionaGachuuri <fionairuka@gmail.com>
2024-10-25 14:12:58 +0300

    models

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -37,7 +37,7 @@ class Base:
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
-        
+
     @staticmethod
     def from_json_string(json_string):
         """Returns the list of the JSON string representation json_string.
@@ -51,14 +51,18 @@ class Base:
         """
         if json_string is None or json_string == "":
             return []
-        return json.loads(json_string)   
+        return json.loads(json_string)
 
     @classmethod
     def save_to_file(cls, list_objs):
         """Writes the JSON string representation of list_objs to a file."""
         if list_objs is None:
             list_objs = []
-        list_dicts = [obj.to_dictionary() for obj in list_objs if hasattr(obj, 'to_dictionary')]
+        list_dicts = [
+            obj.to_dictionary()
+            for obj in list_objs
+            if hasattr(obj, 'to_dictionary')
+            ]
         filename = f"{cls.__name__}.json"
         with open(filename, "w") as file:
             file.write(cls.to_json_string(list_dicts))
