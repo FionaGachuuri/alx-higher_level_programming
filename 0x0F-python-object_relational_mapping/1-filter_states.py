11c4de8a4c767e4d71e6a0f3acd9f84896ff9f4a
FionaGachuuri <fionairuka@gmail.com>
2025-01-02 17:40:30 +0300

    Filter states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Module that  lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa
+Module that  lists all states with a name
+starting with N (upper N) from the database hbtn_0e_0_usa
 """
 import MySQLdb
 import sys
@@ -8,18 +9,24 @@ import sys
 if __name__ == "__main__":
     # Connect to MySQL server
     db = MySQLdb.connect(
-        host="localhost",  # MySQL server is on the same machine
-        port=3306,         # MySQL default port
-        user=sys.argv[1],  # First argument: MySQL username
-        passwd=sys.argv[2],  # Second argument: MySQL password
-        db=sys.argv[3]     # Third argument: Database name
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
     )
 
     # Cursor object to interact with the database
     cursor = db.cursor()
 
     # Execute the query to retrieve states starting with 'N'
-    cursor.execute("SELECT id, name FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    query = """
+    SELECT id, name
+    FROM states
+    WHERE nme LIKE 'N%'
+    ORDER BY id ASC
+    """
+    cursor.execute(query)
 
     # Fetch all the results
     states = cursor.fetchall()
