127dfdbf6cce15456b980956f1df5bf51378a60f
FionaGachuuri <fionairuka@gmail.com>
2025-01-03 13:58:46 +0300

    Filter states by user input

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -11,7 +11,6 @@ if __name__ == "__main__":
     db_name = sys.argv[3]
     state_name = sys.argv[4]
 
-    # Connect to the database
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
@@ -20,21 +19,17 @@ if __name__ == "__main__":
         db=db_name
     )
 
-    # Create a cursor object
+
     cur = db.cursor()
 
-    # SQL query with user input
     query = """SELECT * FROM states WHERE name = '{}'
     ORDER BY id ASC""".format(state_name)
 
-    # Execute the query
     cur.execute(query)
 
-    # Fetch and display results
     rows = cur.fetchall()
     for row in rows:
         print(row)
 
-    # Close the cursor and connection
     cur.close()
     db.close()
