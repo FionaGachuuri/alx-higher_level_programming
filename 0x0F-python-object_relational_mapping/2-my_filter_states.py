de44e1381b987898f902eee951143684c3ea9309
FionaGachuuri <fionairuka@gmail.com>
2025-01-03 13:53:14 +0300

    Filter states by user input

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,20 +1,40 @@
 #!/usr/bin/python3
-"""
-script that takes in an argument and displays all values in
-the states table of hbtn_0e_0_usa where name matches the argument.
-"""
-import MySQLdb
+"""Filter states by user input from the database."""
+
 import sys
+import MySQLdb
 
 if __name__ == "__main__":
-    db = MySQLdb.Connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY "{}" "
-                   .format(sys.argv[4]))
-    results = cursor.fetchall()
-    for row in results:
+    # Retrieve arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    state_name = sys.argv[4]
+
+    # Connect to the database
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=db_name
+    )
+
+    # Create a cursor object
+    cur = db.cursor()
+
+    # SQL query with user input
+    query = """SELECT * FROM states WHERE name = '{}'
+    ORDER BY id ASC""".format(state_name)
+
+    # Execute the query
+    cur.execute(query)
+
+    # Fetch and display results
+    rows = cur.fetchall()
+    for row in rows:
         print(row)
 
-    cursor.close()
+    # Close the cursor and connection
+    cur.close()
     db.close()
