98809881a56c05b6a58734a3368de9bb491cd1f8
FionaGachuuri <fionairuka@gmail.com>
2025-01-21 11:16:44 +0300

    cURL a JSON file

---
+++ 0x10-python-network_0/101-post_json.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Sends a JSON POST request to a URL passed as the first argument,
+# then displays the body of the response. The JSON data is read from a file
+# passed as the second argument.
+
+curl -s -X POST -H "Content-Type: application/json" -d @"$2" "$1"
